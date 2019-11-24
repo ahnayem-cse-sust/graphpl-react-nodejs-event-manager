9af45ae5514517a74a756b4dc1f78e61d8ad2ef0
Nayem <ahnayem.sust@gmail.com>
2019-11-24 20:01:44 +0600

    middleware

--- src/graphql/resolvers/events.js
+++ src/graphql/resolvers/events.js
@@ -1,6 +1,6 @@
 const Event = require('../../models/event');
 const { transformEvent } = require('./merge')
-
+const User = require('../../models/user');
 
 
 module.exports = {
@@ -15,19 +15,22 @@ module.exports = {
             throw err;
         }
     },
-    createEvent: async args => {
+    createEvent: async (args,req) => {
+        if(!req.isAuth){
+            throw new Error('Unauthenticated!');
+        }
         try {
             const event = new Event({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price,
                 date: new Date(args.eventInput.date),
-                creator: '5dd81e75e1bfe224a657427d'
+                creator: req.userId
             })
             let createdEvent;
             const result = await event.save()
             createdEvent = transformEvent(result);
-            const creator = await User.findById('5dd81e75e1bfe224a657427d');
+            const creator = await User.findById(req.userId);
 
             if (!creator) {
                 throw new Error('User not found.');
