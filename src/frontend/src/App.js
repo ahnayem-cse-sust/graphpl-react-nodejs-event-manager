fa5f29a45072b938658f962fc0270cc225051a1d
Nayem <ahnayem.sust@gmail.com>
2019-11-24 22:57:03 +0600

    frontend setup

---
+++ src/frontend/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
+import AuthPage from './pages/Auth';
+import BookingsPage from './pages/Bookings';
+import EventsPage from './pages/Events';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Redirect path="/" to="/auth" exact />
+          <Route path="/auth" component={AuthPage} />
+          <Route path="/events" component={EventsPage} />
+          <Route path="/bookings" component={BookingsPage} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
