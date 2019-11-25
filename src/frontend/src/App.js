179c56deee1301af212be4e2842733193ec65adf
Nayem <ahnayem.sust@gmail.com>
2019-11-25 22:21:44 +0600

    Navbar design

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import AuthPage from './pages/Auth';
 import BookingsPage from './pages/Bookings';
 import EventsPage from './pages/Events';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 import './App.css';
 
@@ -11,12 +12,17 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Switch>
-          <Redirect path="/" to="/auth" exact />
-          <Route path="/auth" component={AuthPage} />
-          <Route path="/events" component={EventsPage} />
-          <Route path="/bookings" component={BookingsPage} />
-        </Switch>
+        <React.Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect path="/" to="/auth" exact />
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/events" component={EventsPage} />
+              <Route path="/bookings" component={BookingsPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
       </BrowserRouter>
     </div>
   );
